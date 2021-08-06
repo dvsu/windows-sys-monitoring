34517c0caf39a739619412c3c7f369a9572bb370
David S <dave.techdev@gmail.com>
2021-08-06 23:48:40 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+import json
+import wmi
+
+
+w = wmi.WMI(namespace="root\OpenHardwareMonitor")
+temperature_infos = w.Sensor()
+
+data = {}
+
+units = {
+    "Clock": "MHz",
+    "Control": "%",
+    "Data": "GB",
+    "Fan": "RPM",
+    "Load": "%",
+    "Power": "W",
+    "Temperature": "C",
+    "Voltage": "V"
+}
+
+for sensor in temperature_infos:
+
+    device_type, device, measurement, *_ = sensor.Identifier.split('/')[1:]
+
+    if device_type not in data.keys():
+        data[device_type] = {}
+
+    if device not in data[device_type].keys():
+        data[device_type][device] = {}
+
+    if measurement not in data[device_type][device].keys():
+        data[device_type][device][measurement] = []
+
+    data[device_type][device][measurement].append({
+        "name": sensor.Name.lower().replace(' ', '_').replace('#', ''),
+        "value": {
+            "current": float(sensor.Value),
+            "min": float(sensor.Min),
+            "max": float(sensor.Max)
+        },
+        "type": sensor.SensorType,
+        "unit": units[sensor.SensorType]
+    })
+
+
+print(json.dumps(data, indent=2))
